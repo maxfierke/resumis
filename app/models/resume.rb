f6b8f0cd217bbe67f7e6e52ab1dd7b6cdf277bf4
Max Fierke <max@maxfierke.com>
2014-11-16 20:40:02 -0600

    Add skills to resume form
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,7 +3,9 @@ class Resume < ActiveRecord::Base
   has_many :resume_education_experiences
   has_many :resume_projects
   has_many :resume_work_experiences
+  has_many :resume_skills
   has_many :education_experiences, through: :resume_education_experiences
   has_many :projects, through: :resume_projects
+  has_many :skills, through: :resume_skills
   has_many :work_experiences, through: :resume_work_experiences
 end
