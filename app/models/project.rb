6a48b7c098be7a9d8a01e1c932959c69aa953d6c
Max Fierke <max@maxfierke.com>
2014-12-04 22:54:59 -0600

    Move project date range logic to model
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,11 @@ class Project < ActiveRecord::Base
   has_many :project_category_joinings
   has_many :project_categories, through: :project_category_joinings
   has_many :resumes, through: :resume_project
+
+  def date_range
+    start_month_year = start_date.strftime('%B %Y')
+    end_month_year = end_date ? end_date.strftime('%B %Y') : 'present'
+
+    "#{start_month_year} — #{end_month_year}"
+  end
 end
