1ac4ea4359f6e6e7d4177ba67ab2dd2376eeacc5
Max Fierke <max@maxfierke.com>
2014-12-04 20:05:51 -0600

    Extract navbar and <head> to partials. Wire up navbar "Resume" link
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 module ApplicationHelper
+
+  def exists_published_resume?
+    Resume.exists?(published: true)
+  end
+
   # TODO: Support multi-tenancy
   def root_user
     User.find(1) rescue nil
+  end
+
+  def path_to_latest_resume
+    latest_resume = Resume.where(published: true).order(updated_at: :desc).first
+
+    return resume_path(latest_resume)
   end
 end
