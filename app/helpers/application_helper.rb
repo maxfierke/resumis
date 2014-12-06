3a9cfc1d7ed6d127777e43204d4bc0a6f58307ba
Max Fierke <max@maxfierke.com>
2014-12-06 13:39:29 -0600

    Add titles to views
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 require 'redcarpet'
 
 module ApplicationHelper
+  def page_title(text)
+    # sets :title content_for in <head>, and outputs an h1 with the title
+    content_for :title, text
+    return content_tag :h1, text
+  end
+
   def markdown(content)
     renderer = Redcarpet::Render::HTML.new(filter_html: true)
     parser = Redcarpet::Markdown.new(renderer, strikethrough: true, underline: true)
