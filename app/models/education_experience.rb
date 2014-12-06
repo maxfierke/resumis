5fa1756fac73eae0ba5de1a62603c28c1aceb20b
Max Fierke <max@maxfierke.com>
2014-12-06 00:28:47 -0600

    Add date_range pseudo property to education and work experience models
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/education_experience.rb
+++ app/models/education_experience.rb
@@ -3,4 +3,11 @@ class EducationExperience < ActiveRecord::Base
   has_many :resumes, through: :resume_education_experience
 
   default_scope { order(start_date: :asc, end_date: :asc) }
+
+  def date_range
+    start_month_year = start_date.strftime('%B %Y')
+    end_month_year = end_date ? end_date.strftime('%B %Y') : 'present'
+
+    "#{start_month_year} — #{end_month_year}"
+  end
 end
--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -3,4 +3,11 @@ class WorkExperience < ActiveRecord::Base
   has_many :resumes, through: :resume_work_experience
 
   default_scope { order(end_date: :desc) }
+
+  def date_range
+    start_month_year = start_date.strftime('%B %Y')
+    end_month_year = end_date ? end_date.strftime('%B %Y') : 'present'
+
+    "#{start_month_year} — #{end_month_year}"
+  end
 end
