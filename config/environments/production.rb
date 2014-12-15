c5522f26819901d77c08119aaaf1e5be563f8d5e
Max Fierke <max@maxfierke.com>
2014-12-14 21:51:10 -0600

    Use sendgrid in production
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,3 +76,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "587",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN'],
+  :enable_starttls_auto => true
+}
