6ea0db54d23fe5565d3244d27c564c6416b3c122
Max Fierke <max@maxfierke.com>
2014-12-31 20:05:05 -0600

    Whoops. Need to do ternary here.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/initializers/resumis.rb
+++ config/initializers/resumis.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Sets the tenancy mode of the application. Possible values: :single, :multi
-  config.x.resumis.tenancy_mode = ENV['RESUMIS_TENANCY_MODE'].to_sym || :single
+  config.x.resumis.tenancy_mode = ENV['RESUMIS_TENANCY_MODE'] ? ENV['RESUMIS_TENANCY_MODE'].to_sym : :single
 
   # Used for ensuring authentication routes and such point to the right place
   config.x.resumis.canonical_host = ENV['RESUMIS_CANONICAL_HOST'] || 'lvh.me'
