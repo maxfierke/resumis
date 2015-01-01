327d6e49482a3d63fdcbd7eae6973ac05b5b5497
Max Fierke <max@maxfierke.com>
2014-12-31 19:57:58 -0600

    Derp. ENV access needs to come before fallbacks
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/initializers/resumis.rb
+++ config/initializers/resumis.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Sets the tenancy mode of the application. Possible values: :single, :multi
-  config.x.resumis.tenancy_mode = :single || ENV['RESUMIS_TENANCY_MODE'].to_sym
+  config.x.resumis.tenancy_mode = ENV['RESUMIS_TENANCY_MODE'].to_sym || :single
 
   # Used for ensuring authentication routes and such point to the right place
-  config.x.resumis.canonical_host = 'lvh.me' || ENV['RESUMIS_CANONICAL_HOST']
+  config.x.resumis.canonical_host = ENV['RESUMIS_CANONICAL_HOST'] || 'lvh.me'
 end
