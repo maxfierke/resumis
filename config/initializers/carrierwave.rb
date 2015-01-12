4f7b7098d73a89ee35f7119c24ee019a70064d61
Max Fierke <max@maxfierke.com>
2015-01-12 15:08:03 -0600

    Only set carrierwave fog parameters under production
    Fixes some issues with eager loading
    
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
+  if Rails.env.production?
+    config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['S3_REGION']
-  }
-  config.fog_directory = ENV['S3_BUCKET_NAME']
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
 
-  if Rails.env.production?
     config.storage = :fog
   else
     config.storage = :file
