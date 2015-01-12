b1cc946c759d9e0138177f6a0134431e23ac7ee0
Max Fierke <max@maxfierke.com>
2015-01-12 11:12:26 -0600

    Use camelCase in JBuilder output
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Jbuilder.key_format camelize: :lower
+
 # Initialize the Rails application.
 Rails.application.initialize!
