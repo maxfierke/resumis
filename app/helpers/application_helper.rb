819e74b0a7018d22612e01b33c32d4eff5bb5040
Max Fierke <max@maxfierke.com>
2015-03-19 23:33:44 -0500

    blergghfd tired forgot to commit these changes to application_helper asdasddfosdaop asdfasfd dasfasf fds fd sdf
    fuucckk
    
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 require 'redcarpet'
 
 module ApplicationHelper
-  def page_title(text, tag_type = :h1, tag_attributes = {})
+  def page_title(text, tag_type = :h1, tag_attributes = {}, prepend_name = true)
     # sets :title content_for in <head>, and outputs an h1 with the title
-    if current_tenant
+    if current_tenant && prepend_name
       content_for :title, "#{current_tenant.full_name} - #{text}"
     else
       content_for :title, text
     end
 
-    return content_tag tag_type, text, tag_attributes
+    content_tag tag_type, text, tag_attributes
   end
 
   def markdown(content)
@@ -17,6 +17,10 @@ module ApplicationHelper
     renderer = Redcarpet::Render::HTML.new(filter_html: true)
     parser = Redcarpet::Markdown.new(renderer, strikethrough: true, underline: true)
     parser.render(content).html_safe
+  end
+
+  def exists_published_blog_posts?
+    Post.exists?(published: true)
   end
 
   def exists_published_resume?
@@ -30,6 +34,10 @@ module ApplicationHelper
     else
       canonical_host
     end
+  end
+
+  def post_categories
+    PostCategory.all
   end
 
   def google_client_id
