62d3022a7421c8db6016e8b8e3977bf37650c395
Max Fierke <max@maxfierke.com>
2015-03-19 23:35:26 -0500

    Set sidekiq as the activejob queue
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Resumis
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # activejob queue adapter
+    config.active_job.queue_adapter = :sidekiq
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
