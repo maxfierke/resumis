42445f61e9105cee23404166ddfaf45ae29a91c6
Max Fierke <max@maxfierke.com>
2015-03-27 14:00:54 -0500

    Add presence and length validators for necessary fields on education_experience
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/education_experience.rb
+++ app/models/education_experience.rb
@@ -6,6 +6,10 @@ class EducationExperience < ActiveRecord::Base
 
   default_scope { order(start_date: :asc, end_date: :asc) }
 
+  validates :school_name, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :diploma, presence: true, length: { minimum: 2, maximum: 255 }
+  validates :start_date, presence: true
+
   def date_range
     start_month_year = start_date.strftime('%B %Y')
     end_month_year = end_date ? end_date.strftime('%B %Y') : 'present'
