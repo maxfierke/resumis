cc0bf7a7e735f7b871e22cb0958ca569958aa053
Max Fierke <max@maxfierke.com>
2015-03-27 12:31:31 -0500

    Use ActiveJob for devise user emails
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -98,4 +98,8 @@ class User < ActiveRecord::Base
 
     networks
   end
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
