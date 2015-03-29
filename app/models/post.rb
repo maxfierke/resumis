75f16f9bdc522ebf784f49b2304b737352035ec6
Max Fierke <max@maxfierke.com>
2015-03-29 18:45:24 -0500

    Remove validator for removed post#tagline property
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,5 +11,4 @@ class Post < ActiveRecord::Base
   has_many :post_categories, through: :post_category_joinings
 
   validates :title, presence: true, uniqueness: { scope: :user_id }, length: { minimum: 3, maximum: 60 }
-  validates :tagline, length: { maximum: 40 }
 end
