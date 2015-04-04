270fe26f14d931618c00577e50926e884c7c1bfb
Max Fierke <max@maxfierke.com>
2015-04-04 12:37:29 -0500

    Ensure ActsAsTenant.current_tenant is nil after every test
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.after(:each) do
+    ActsAsTenant.current_tenant = nil
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
