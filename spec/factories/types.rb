149c63d56eca134b50d5c3f446fb4518b2c94110
Max Fierke <max@maxfierke.com>
2015-05-06 22:38:29 -0500

    Build out Type spec
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ spec/factories/types.rb
@@ -0,0 +1,8 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :type do
+    name { Faker::Lorem.word }
+    slug { Faker::Lorem.word }
+  end
+end
--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Type, type: :model do
-  it 'has a valid factory'
-  it 'is invalid without a name'
-  it 'is invalid without a unique slug'
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:type)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    expect(FactoryGirl.build(:type, name: nil)).not_to be_valid
+  end
+
+  it 'is invalid without a unique slug' do
+    first_type = FactoryGirl.create(:type)
+
+    expect(FactoryGirl.build(:type, slug: first_type.slug)).not_to be_valid
+  end
 end
