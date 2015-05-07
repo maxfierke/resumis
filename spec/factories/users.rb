74d61b2080c21cdc12e04ad2b9215b14fc1d6d72
Max Fierke <max@maxfierke.com>
2015-05-06 22:27:20 -0500

    Build out User spec
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    subdomain { Faker::Internet.domain_word }
+    password { Faker::Internet.password }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,52 @@
 require 'rails_helper'
+require 'digest'
 
 RSpec.describe User, type: :model do
-  it 'has a valid factory'
-  it 'is invalid without a first_name'
-  it 'is invalid without a last_name'
-  it 'is invalid without a subdomain in multi-tenancy mode'
-  it 'needs a unique subdomain in multi-tenancy mode'
-  it 'can not use a reserved subdomain as its subdomain'
-  it 'returns a full_name as a concat. of first_name and last_name'
-  it 'returns a gravatar url from its email'
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it 'is invalid without a first_name' do
+    expect(FactoryGirl.build(:user, first_name: nil)).not_to be_valid
+  end
+
+  it 'is invalid without a last_name' do
+    expect(FactoryGirl.build(:user, last_name: nil)).not_to be_valid
+  end
+
+  it 'is invalid without a subdomain in multi-tenancy mode' do
+    expect(Rails.application.config.x.resumis.tenancy_mode).to eq(:multi)
+    expect(FactoryGirl.build(:user, subdomain: nil)).not_to be_valid
+  end
+
+  it 'needs a unique subdomain in multi-tenancy mode' do
+    expect(Rails.application.config.x.resumis.tenancy_mode).to eq(:multi)
+    first_user = FactoryGirl.create(:user)
+
+    expect(FactoryGirl.build(:user, subdomain: first_user.subdomain)).not_to be_valid
+  end
+
+  it 'can not use a reserved subdomain as its subdomain' do
+    expect(FactoryGirl.build(:user, subdomain: 'accounts')).not_to be_valid
+  end
+
+  it 'returns a full_name as a concat. of first_name and last_name' do
+    user = FactoryGirl.create(:user)
+
+    expect(user.full_name).to eq(user.first_name + ' ' + user.last_name)
+  end
 
   describe '#copyright_range' do
-    it 'returns only the current year if only one year has spanned'
-    it 'returns the full span of years if more than a year old'
+    it 'returns only the current year if only one year has spanned since creation' do
+      user = FactoryGirl.create(:user, created_at: DateTime.now)
+
+      expect(user.copyright_range).to eq("#{DateTime.now.year} #{user.full_name}")
+    end
+
+    it 'returns the full span of years if more than a year old' do
+      user = FactoryGirl.create(:user, created_at: DateTime.new(2001,2,3))
+
+      expect(user.copyright_range).to eq("2001-#{DateTime.now.year} #{user.full_name}")
+    end
   end
 end
