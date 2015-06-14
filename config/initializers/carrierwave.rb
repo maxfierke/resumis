45fb062727f4def83ce1addcbbbab55b17f58f19
Max Fierke <max@maxfierke.com>
2015-06-14 12:45:06 -0500

    Use path-style S3 URLs to get around cert commonName mismatch errors
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['S3_REGION']
+      region: ENV['S3_REGION'],
+      path_style: true
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
 
