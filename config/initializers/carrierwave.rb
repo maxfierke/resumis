f767b52b321840e1b0100647e9e5a40b86d80493
Max Fierke <max@maxfierke.com>
2015-06-14 19:07:33 -0500

    Set asset host for path-style S3 URLs
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,10 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['S3_REGION'],
-      path_style: true
+      region: ENV['S3_REGION']
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.asset_host    = "https://s3.amazonaws.com/#{config.fog_directory}"
 
     config.storage = :fog
   else
