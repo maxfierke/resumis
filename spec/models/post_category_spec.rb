5a291bd2183693d1645eaa95cd58c7bbd4a2868f
Max Fierke <max@maxfierke.com>
2015-10-06 13:13:55 -0500

    Fix post category tests
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PostCategory, type: :model do
+  before { ActsAsTenant.current_tenant = FactoryGirl.create :user }
+  after { ActsAsTenant.current_tenant = nil }
+
   it 'has a valid factory' do
     expect(FactoryGirl.create(:post_category)).to be_valid
   end
