d146e75bc625ed8c0df49807181d53140f0da265
Max Fierke <max@maxfierke.com>
2015-10-09 18:07:16 -0500

    protect against non-valid Faker generated data
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/factories/education_experiences.rb
+++ spec/factories/education_experiences.rb
@@ -2,8 +2,8 @@ require 'faker'
 
 FactoryGirl.define do
   factory :education_experience do
-    school_name { Faker::Name.name + ' University' }
-    diploma { Faker::Lorem.word }
+    school_name { Faker::Name.name[0..200] + ' University' }
+    diploma { Faker::Lorem.words(4)[0..130] }
     start_date { Faker::Time.between(15.years.ago, 2.years.ago, :midnight) }
   end
 end
--- spec/factories/post_categories.rb
+++ spec/factories/post_categories.rb
@@ -2,6 +2,6 @@ require 'faker'
 
 FactoryGirl.define do
   factory :post_category do
-    name { Faker::Name.name }
+    name { Faker::Lorem.words(4)[0..75] }
   end
 end
