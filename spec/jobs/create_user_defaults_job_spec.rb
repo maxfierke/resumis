2102820a466bc9023155b88eeb324dd517fa806d
Max Fierke <max@maxfierke.com>
2015-10-09 22:04:14 -0500

    Add tests for CreateUserDefaultsJob
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/jobs/create_user_defaults_job_spec.rb
+++ spec/jobs/create_user_defaults_job_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe CreateUserDefaultsJob, :type => :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should create standard project statuses' do
+    user = FactoryGirl.create(:user)
+
+    CreateUserDefaultsJob.new().perform(user)
+
+    ActsAsTenant.with_tenant(user) do
+      expect(ProjectStatus.count).to eql(4)
+    end
+  end
+
+  context 'developers' do
+    let(:developer_type) { FactoryGirl.create(:type, slug: 'developer') }
+
+    it 'should create default developer skill categories' do
+      user = FactoryGirl.create(:user, types: [developer_type])
+
+      CreateUserDefaultsJob.new().perform(user)
+
+      ActsAsTenant.with_tenant(user) do
+        expect(SkillCategory.count).to eql(5)
+      end
+    end
+  end
 end
