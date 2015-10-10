f94f55d99450a151fac79a460796d8eac4263f3b
Max Fierke <max@maxfierke.com>
2015-10-09 21:20:26 -0500

    Build out pending SkillCategory model specs
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/factories/skill_categories.rb
+++ spec/factories/skill_categories.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :skill_category do
-    
+    name { Faker::Lorem.words(3) }
   end
-
 end
--- spec/models/skill_category_spec.rb
+++ spec/models/skill_category_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe SkillCategory, type: :model do
-  it 'has a valid factory'
-  it 'is invalid without a name'
-  it 'is unique to a tenanted user'
+  before { ActsAsTenant.current_tenant = FactoryGirl.create :user }
+  after { ActsAsTenant.current_tenant = nil }
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:skill_category)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    expect(FactoryGirl.build(:skill_category, name: nil)).not_to be_valid
+  end
+
+  it 'is unique to a tenanted user' do
+    FactoryGirl.create(:skill_category, name: 'I am a cat')
+
+    expect(FactoryGirl.build(:skill_category, name: 'I am a cat')).not_to be_valid
+  end
 end
