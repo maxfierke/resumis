74a5b8b7d7ecab7f3e526b9805ce0557781c0767
Max Fierke <max@maxfierke.com>
2015-10-09 19:29:20 -0500

    Add end_date to EducationExperience factory
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/factories/education_experiences.rb
+++ spec/factories/education_experiences.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     school_name { Faker::Name.name[0..200] + ' University' }
     diploma { Faker::Lorem.words(4)[0..130] }
     start_date { Faker::Time.between(15.years.ago, 2.years.ago, :midnight) }
+    end_date { Faker::Time.between(2.years.ago, Time.now, :midnight) }
   end
 end
