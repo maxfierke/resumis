036f45fbf49bd160cee096259db87369b6f9b841
Max Fierke <max@maxfierke.com>
2015-10-09 21:15:35 -0500

    build out pending ProjectStatus specs
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/factories/project_statuses.rb
+++ spec/factories/project_statuses.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :project_status do
-    
+    name { Faker::Lorem.word }
   end
-
 end
--- spec/models/project_status_spec.rb
+++ spec/models/project_status_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ProjectStatus, type: :model do
-  it 'has a valid factory'
-  it 'is invalid without a unique name'
-  it 'is unique to a tenant user'
+  before { ActsAsTenant.current_tenant = FactoryGirl.create :user }
+  after { ActsAsTenant.current_tenant = nil }
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:project_status)).to be_valid
+  end
+
+  it 'is invalid without a unique name' do
+    FactoryGirl.create(:project_status, name: 'I am a post')
+
+    expect(FactoryGirl.build(:project_status, name: 'I am a post')).not_to be_valid
+  end
+
+  it 'is invalid without a unique slug' do
+    FactoryGirl.create(:project_status, slug: 'test-status')
+
+    expect(FactoryGirl.build(:project_status, slug: 'test-status')).not_to be_valid
+  end
 end
