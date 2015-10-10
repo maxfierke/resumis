0c67d38d31b525d375f523c36f1cbf162ddd0109
Max Fierke <max@maxfierke.com>
2015-10-09 21:34:12 -0500

    build out pending WorkExperience model specs
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- spec/factories/work_experiences.rb
+++ spec/factories/work_experiences.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :work_experience do
-    
+    organization { Faker::Name.name[0...255] + ' Inc.' }
+    position { Faker::Lorem.words(4)[0...255] }
+    start_date { Faker::Time.between(15.years.ago, 2.years.ago, :midnight) }
+    end_date { Faker::Time.between(2.years.ago, Time.now, :midnight) }
   end
 
 end
--- spec/models/work_experience_spec.rb
+++ spec/models/work_experience_spec.rb
@@ -1,17 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe WorkExperience, type: :model do
-  it 'has a valid factory'
-  it 'is invalid without an organization'
-  it 'is invalid with an organization less than 2 characters'
-  it 'is invalid with an organization greater than 255 characters'
-  it 'is invalid without a position'
-  it 'is invalid with a position less than 3 characters'
-  it 'is invalid with a position greater than 255 characters'
-  it 'is invalid without a start_date'
+  before { ActsAsTenant.current_tenant = FactoryGirl.create :user }
+  after { ActsAsTenant.current_tenant = nil }
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:work_experience)).to be_valid
+  end
+
+  it 'is invalid without an organization' do
+    expect(FactoryGirl.build(:work_experience, organization: nil)).not_to be_valid
+  end
+
+  it 'is invalid with an organization less than 2 characters' do
+    expect(FactoryGirl.build(:work_experience, organization: 'M')).not_to be_valid
+  end
+
+  it 'is invalid with an organization greater than 255 characters' do
+    expect(FactoryGirl.build(:work_experience, organization: Faker::Lorem.characters(256))).not_to be_valid
+  end
+
+  it 'is invalid without a position' do
+    expect(FactoryGirl.build(:work_experience, position: nil)).not_to be_valid
+  end
+
+  it 'is invalid with a position less than 3 characters' do
+    expect(FactoryGirl.build(:work_experience, position: 'aa')).not_to be_valid
+  end
+
+  it 'is invalid with a position greater than 255 characters' do
+    expect(FactoryGirl.build(:work_experience, position: Faker::Lorem.characters(256))).not_to be_valid
+  end
+
+  it 'is invalid without a start_date' do
+    expect(FactoryGirl.build(:work_experience, start_date: nil)).not_to be_valid
+  end
 
   describe '#date_range' do
-    it "returns 'Month Year - present' without an end_date"
-    it "returns 'Month Year - Month Year' with an end_date"
+    it "returns 'Month Year - present' without an end_date" do
+      work_exp = FactoryGirl.create(:work_experience,
+                                    start_date: DateTime.new(2014,1,1),
+                                    end_date: nil)
+
+      expect(work_exp.date_range).to eq("January 2014 — present")
+    end
+
+    it "returns 'Month Year - Month Year' with an end_date" do
+      work_exp = FactoryGirl.create(:work_experience,
+                                    start_date: DateTime.new(2013,4,1),
+                                    end_date: DateTime.new(2015,8,16))
+
+      expect(work_exp.date_range).to eq("April 2013 — August 2015")
+    end
   end
 end
