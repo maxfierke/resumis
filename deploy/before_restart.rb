2ec5c32a03c5a253e1d7e2d8a8b97bdd2ea0abcc
Max Fierke <max@maxfierke.com>
2017-09-24 10:55:20 -0500

    Remove old OpsWorks deploy hook

--- deploy/before_restart.rb
+++
@@ -1,7 +0,0 @@
-node[:deploy].each do |application, deploy|
-  if application == 'resumis' # replace api with your app
-    run "bundle exec rake sidekiq:start"
-  end
-end
-
-run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
