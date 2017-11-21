b6b49d2207677fb2e2f6392885bf609c33e296b5
Max Fierke <max@maxfierke.com>
2017-11-20 20:10:59 -0600

    Add user policy to Manage::ProfileController

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,4 +2,8 @@ class UserPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def update?
+    user.admin? || user == record
+  end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe UserPolicy do
       let(:policy_user) { PolicyUser.new(nil) }
 
       it { is_expected.to permit_action(:show) }
+      it { is_expected.to forbid_actions([:edit, :update]) }
     end
 
     context 'user is an API user' do
@@ -26,14 +27,40 @@ RSpec.describe UserPolicy do
       end
       let(:user) { FactoryGirl.create(:user) }
 
-      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_actions([:show]) }
+      it { is_expected.to forbid_actions([:edit, :update]) }
+
+      context 'user is admin' do
+        let(:test_user) { FactoryGirl.create(:user, admin: true) }
+
+        it { is_expected.to permit_actions([:show, :edit, :update]) }
+      end
+
+      context 'authenticated user is the same as the user resource' do
+        let(:user) { test_user }
+
+        it { is_expected.to permit_actions([:show, :edit, :update]) }
+      end
     end
 
     context 'user is a browser user' do
       let(:policy_user) { PolicyUser.new(test_user) }
       let(:user) { FactoryGirl.create(:user) }
 
-      it { is_expected.to permit_action(:show) }
+      it { is_expected.to permit_actions([:show]) }
+      it { is_expected.to forbid_actions([:edit, :update]) }
+
+      context 'user is admin' do
+        let(:test_user) { FactoryGirl.create(:user, admin: true) }
+
+        it { is_expected.to permit_actions([:show, :edit, :update]) }
+      end
+
+      context 'authenticated user is the same as the user resource' do
+        let(:user) { test_user }
+
+        it { is_expected.to permit_actions([:show, :edit, :update]) }
+      end
     end
   end
 end
