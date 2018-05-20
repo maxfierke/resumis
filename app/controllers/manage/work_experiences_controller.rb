0c6cbc1c4229c3c5c7c628b3fd936e799b7659b3
Max Fierke <max@maxfierke.com>
2018-05-20 13:17:02 -0500

    Add missing authorize calls to work experiences controller

--- app/controllers/manage/work_experiences_controller.rb
+++ app/controllers/manage/work_experiences_controller.rb
@@ -5,12 +5,13 @@ module Manage
     respond_to :html, :json
 
     def index
-      @work_experiences = WorkExperience.page params[:page]
+      @work_experiences = policy_scope(WorkExperience).page params[:page]
       respond_with(@work_experiences)
     end
 
     def new
       @work_experience = WorkExperience.new
+      authorize @work_experience
       respond_with(@work_experience, :location => manage_work_experiences_path)
     end
 
@@ -19,7 +20,7 @@ module Manage
 
     def create
       @work_experience = WorkExperience.new(work_experience_params)
-      @work_experience.user = current_user
+      authorize @work_experience
       @work_experience.save
       respond_with(@work_experience, :location => manage_work_experiences_path)
     end
@@ -36,11 +37,18 @@ module Manage
 
     private
       def set_work_experience
-        @work_experience = WorkExperience.find(params[:id])
+        @work_experience = policy_scope(WorkExperience).find(params[:id])
+        authorize @work_experience
       end
 
       def work_experience_params
-        params.require(:work_experience).permit(:organization, :position, :description, :start_date, :end_date)
+        params.require(:work_experience).permit(
+          :organization,
+          :position,
+          :description,
+          :start_date,
+          :end_date
+        ).merge!(user: current_user)
       end
   end
 end
