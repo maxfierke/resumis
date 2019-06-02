888cbb11073be8d503fe806f95ddb73136d239eb
Max Fierke <max@maxfierke.com>
2019-06-01 22:58:06 -0500

    Add lockable to devise user

---
+++ db/migrate/20190602022245_add_lockable_to_devise.rb
@@ -0,0 +1,6 @@
+class AddLockableToDevise < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :failed_attempts, :integer, default: 0, null: false
+    add_column :users, :locked_at, :datetime
+  end
+end
