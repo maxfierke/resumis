04214fd4172ce032362823c38d0566d82ead113a
Max Fierke <max@maxfierke.com>
2019-08-24 23:52:41 -0500

    Fix travis

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
-  resume.css pdf.css manage.css manage.js
+  resume.css pdf.css
 )
