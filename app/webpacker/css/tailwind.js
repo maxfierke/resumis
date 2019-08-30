862c5402e839d657d9c3443f8817ca940f1d8106
Max Fierke <max@maxfierke.com>
2019-08-29 23:47:34 -0500

    Remove custom theme colors until I can settle on something more granular

--- app/webpacker/css/tailwind.js
+++ app/webpacker/css/tailwind.js
@@ -1,42 +1,6 @@
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        'black-light': '#212122',
-
-        'grey-lightest': '#f9f9fa',
-        'grey-lighter': '#e5e6e6',
-        'grey-light': '#d1d2d2',
-        'grey': '#404040',
-        'grey-dark': '#333333',
-        'grey-darker': '#2b2b2b',
-        'grey-darkest': '#222222',
-
-        'brand-light': '#d7eefe',
-        'brand': '#18befb',
-        'brand-dark': '#235b75',
-
-        'cta-light': '#ffd0cf',
-        'cta': '#fb184d',
-        'cta-dark': '#781e28',
-
-        'info-light': '#daf5ff',
-        'info': '#2fd7fe',
-        'info-dark': '#286577',
-
-        'warning-light': '#fdf4d6',
-        'warning': '#ead659',
-        'warning-dark': '#6f6530',
-
-        'success-light': '#d9fadb',
-        'success': '#4ae471',
-        'success-dark': '#2d6c39',
-
-        'danger-light': '#ffd8d7',
-        'danger': '#ee5f67',
-        'danger-dark': '#713234',
-      }
-    }
+    extend: {}
   },
   variants: {},
   plugins: [
