2d7a1430e7c463da08bf3ad297784e1771c27a49
Max Fierke <max@maxfierke.com>
2019-08-29 21:16:08 -0500

    Move webpacker stuff to app/webpacker

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('tailwindcss')('./app/webpacker/css/tailwind.js'),
     require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
