a5e0732e778422925d4a7e703755b7dbd7a060fe
Max Fierke <max@maxfierke.com>
2019-10-23 23:26:28 -0500

    Update rails gem to 6.0

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,15 @@
 require_relative 'boot'
 require_relative 'initializers/resumis'
 
+require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
