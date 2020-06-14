d898ac31dfb85c6fe5b89a7987278b324cbac387
Max Fierke <max@maxfierke.com>
2020-06-14 15:14:13 -0500

    Set SameSite=lax & HttpOnly on session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_resumis_session', domain: :all
+Rails.application.config.session_store :cookie_store,
+  key: '_resumis_session',
+  domain: :all,
+  same_site: :lax,
+  httponly: true
