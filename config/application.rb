16b44c27155921139db384fb5155547cde57997e
Max Fierke <max@maxfierke.com>
2021-09-23 22:28:31 -0500

    Opt in to Rails 6.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Resumis
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
