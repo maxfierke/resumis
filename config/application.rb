9d02d7abaef6fd0d4f4937a77d5de4008a2e4a50
Max Fierke <max@maxfierke.com>
2022-05-22 17:36:51 -0500

    Update Rails configs for Rails 7

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
-require_relative 'boot'
-require_relative 'initializers/resumis'
+require_relative "boot"
+require_relative "initializers/resumis"
 
 require "rails"
 require "active_model/railtie"
@@ -12,7 +12,6 @@ require "action_mailer/railtie"
 # require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,10 +20,7 @@ Bundler.require(*Rails.groups)
 
 module Resumis
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
     config.middleware.insert_before 0, Rack::Cors do
