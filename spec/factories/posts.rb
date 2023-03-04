afd3dc288dd84624bdf21e0f628816ed0f5899e4
Max Fierke <max@maxfierke.com>
2023-03-04 14:50:29 -0600

    Add specs for post categories in RSS and Atom feeds

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,20 @@ FactoryBot.define do
     sequence(:title) { |n| "#{Faker::Lorem.sentence(word_count: 2)[0...55]}#{n}" }
     body { Faker::Markdown.sandwich(sentences: 6, repeat: 3) }
     association(:user)
+
+    trait :published do
+      published { true }
+      published_on { rand(1..24).months.ago }
+    end
+
+    trait :with_categories do
+      transient do
+        category_count { rand(5) }
+      end
+
+      after(:build) do |post, evaluator|
+        post.post_categories = build_list(:post_category, evaluator.category_count)
+      end
+    end
   end
 end
--- spec/requests/feeds_spec.rb
+++ spec/requests/feeds_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Feeds", type: :request do
   let!(:user) do
-    FactoryBot.create(:user,
-      tagline: "I write words!",
-      blog_url: "https://blogs.biz"
-    )
+    FactoryBot.create(:user)
   end
 
-  before { ActsAsTenant.current_tenant = user }
-  after { ActsAsTenant.current_tenant = nil }
+  before do
+    ActsAsTenant.test_tenant = user
+  end
 
   describe "GET /posts.rss", :aggregate_failures do
     it "returns an RSS feed w/ the expected posts" do
-      FactoryBot.create_list(:post, 4, published_on: Time.current, published: true, user: user)
-      posts = user.posts.order(
+      FactoryBot.create_list(:post, 4, :with_categories, :published)
+      posts = user.posts.includes(:post_categories).order(
         published_on: :desc,
         updated_at: :desc,
         created_at: :desc
@@ -31,7 +29,7 @@ RSpec.describe "Feeds", type: :request do
       expect(channel.xpath("./title").first.content).to eq("#{user.full_name}'s Blog")
       expect(channel.xpath("./copyright").first.content).to eq("Copyright © #{Time.current.year} #{user.full_name}")
       expect(channel.xpath("./description").first.content).to eq(user.tagline)
-      expect(channel.xpath("./link").first.content).to eq("https://blogs.biz")
+      expect(channel.xpath("./link").first.content).to eq(user.blog_url)
 
       post_items = channel.xpath("./item")
       expect(post_items.size).to eq(posts.size)
@@ -41,13 +39,17 @@ RSpec.describe "Feeds", type: :request do
         expect(post_item.xpath("./title").first.content).to eq(post.title)
         expect(post_item.xpath("./author").first.content).to eq(user.full_name)
         expect(post_item.xpath("./pubDate").first.content).to eq(post.published_on.to_fs(:rfc822))
-        expect(post_item.xpath("./link").first.content).to eq("https://blogs.biz/#{post.slug}")
+        expect(post_item.xpath("./link").first.content).to eq("#{user.blog_url}/#{post.slug}")
         expect(post_item.xpath("./description").first.content).to include(ApplicationHelper.markdown(post.body))
+
+        rss_categories = post_item.xpath("./category").map(&:content)
+
+        expect(rss_categories).to match_array(post.post_categories.pluck(:name))
       end
     end
 
     it "only returns the latest 15" do
-      FactoryBot.create_list(:post, 25, published_on: Time.current, published: true, user: user)
+      FactoryBot.create_list(:post, 25, :with_categories, :published)
 
       get posts_feed_path(format: :rss)
       expect(response).to have_http_status(200)
@@ -60,8 +62,8 @@ RSpec.describe "Feeds", type: :request do
 
   describe "GET /posts.atom", :aggregate_failures do
     it "returns an Atom feed w/ the expected posts" do
-      FactoryBot.create_list(:post, 4, published_on: Time.current, published: true, user: user)
-      posts = user.posts.order(
+      FactoryBot.create_list(:post, 4, :with_categories, :published)
+      posts = user.posts.includes(:post_categories).order(
         published_on: :desc,
         updated_at: :desc,
         created_at: :desc
@@ -79,7 +81,7 @@ RSpec.describe "Feeds", type: :request do
       expect(feed.xpath("./xmlns:subtitle").first.content).to eq(user.tagline)
       expect(feed.xpath("./xmlns:author/xmlns:name").first.content).to eq(user.full_name)
       expect(feed.xpath("./xmlns:rights").first.content).to eq("Copyright © #{Time.current.year} #{user.full_name}")
-      expect(feed.xpath("./xmlns:link[@rel='alternate']").first["href"]).to eq("https://blogs.biz")
+      expect(feed.xpath("./xmlns:link[@rel='alternate']").first["href"]).to eq(user.blog_url)
 
       post_items = feed.xpath("./xmlns:entry")
       expect(post_items.size).to eq(posts.size)
@@ -90,15 +92,19 @@ RSpec.describe "Feeds", type: :request do
         expect(post_item.xpath("./xmlns:author/xmlns:name").first.content).to eq(user.full_name)
         expect(post_item.xpath("./xmlns:published").first.content).to eq(post.published_on.iso8601)
         expect(post_item.xpath("./xmlns:updated").first.content).to eq(post.updated_at.iso8601)
-        expect(post_item.xpath("./xmlns:link").first["href"]).to eq("https://blogs.biz/#{post.slug}")
+        expect(post_item.xpath("./xmlns:link").first["href"]).to eq("#{user.blog_url}/#{post.slug}")
         content_node = post_item.xpath("./xmlns:content").first
         expect(content_node["type"]).to eq("html")
         expect(content_node.content).to include(ApplicationHelper.markdown(post.body))
+
+        post.post_categories.each do |post_category|
+          expect(post_item.xpath("./xmlns:category[@term='#{post_category.name}' and @label='#{post_category.name}']")).not_to be_empty
+        end
       end
     end
 
     it "only returns the latest 15" do
-      FactoryBot.create_list(:post, 25, published_on: Time.current, published: true, user: user)
+      FactoryBot.create_list(:post, 25, :with_categories, :published)
 
       get posts_feed_path(format: :atom)
       expect(response).to have_http_status(200)
